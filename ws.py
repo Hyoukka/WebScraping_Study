e5aa1e904b04dbdc7177cbb4dd1415d8ee0b1696
Cassius Vinicius <cassiusoliveira10@gmail.com>
2023-02-22 21:14:35 -0400

    Learned about searching and filtering

--- ws.py
+++ ws.py
@@ -1,1 +1,12 @@
-"first commit"
+from bs4 import BeautifulSoup
+import re
+
+with open("index.html", "r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+
+tags = doc.find_all("input", type="text")
+for tag in tags:
+    tag['placeholder'] = "I changed you"
+
+with open("change.html", "w") as file:
+    file.write(str(doc))
