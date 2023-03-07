2e4402d546b46033209ebbc973762ae9523255b0
Cassius <cassiusoliveira10@gmail.com>
2023-03-07 09:37:34 -0400

    ActionsChains and Automating Cookie Clicker

--- Selenium/tutorial.py
+++ Selenium/tutorial.py
@@ -1,38 +1,30 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 service = Service("/home/cassius/Downloads/chromedriver")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://techwithtim.net")
+driver.get("https://orteil.dashnet.org/cookieclicker/")
 
+driver.implicitly_wait(30)
 
-link = driver.find_element(By.LINK_TEXT, "Python Programming")
-link.click()
+for i in range(5000):
+    cookie = driver.find_element(By.ID, "bigCookie")
+    cookie_count = driver.find_element(By.ID, "cookies")
 
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
-    )
-    element.click()
+    items = [driver.find_element(By.ID, "productPrice" + str(i)) for i in range(1,-1,-1)]
 
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "sow-button-19310003"))
-    )
-    element.click()
-
-    driver.back()
-    driver.back()
-    driver.back()
-    driver.forward()
-    driver.forward()
-
-except:
-    driver.quit()
-
-time.sleep(60)
+    actions = ActionChains(driver)
+    actions.click(cookie)
+    actions.perform()
+    count = int(cookie_count.text.split(" ")[0])
+    for item in items:
+        value = int(item.text)
+        if value <= count:
+            upgrade_actions = ActionChains(driver)
+            upgrade_actions.move_to_element(item)
+            upgrade_actions.click()
+            upgrade_actions.perform()
